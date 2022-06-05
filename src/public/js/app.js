cb9fe32b338c2495b4797ef0dbd60124294b6023
imhynni <omelas329@gmail.com>
2022-06-05 18:39:39 +0900

    use media stream

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,6 @@
 const socket = io();
 
+// chat
 const welcome = document.getElementById("welcome");
 const nickDiv = welcome.querySelector("#nickname");
 const nickForm = nickDiv.querySelector("form");
@@ -86,3 +87,95 @@ socket.on("room_change", (rooms) => {
     roomList.appendChild(li);
   });
 });
+
+// video call
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach((camera) => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if (currentCamera.label == camera.label) {
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getMedia(deviceId) {
+  // deviceId가 없을 때 실행
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  // devicedId가 있을 때 실행
+  const cameraConstrains = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstrains : initialConstrains
+    );
+    myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+getMedia();
+
+function handleMuteClick() {
+  console.log(
+    myStream
+      .getAudioTracks()
+      .forEach((track) => (track.enabled = !track.enabled))
+  );
+  if (muted) {
+    muteBtn.innerText = "음소거";
+    muted = false;
+  } else {
+    muteBtn.innerText = "음소거 해제";
+    muted = true;
+  }
+}
+function handleCameraClick() {
+  console.log(
+    myStream
+      .getVideoTracks()
+      .forEach((track) => (track.enabled = !track.enabled))
+  );
+  if (cameraOff) {
+    cameraBtn.innerText = "비디오 끄기";
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = "비디오 켜기";
+    cameraOff = true;
+  }
+}
+
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
